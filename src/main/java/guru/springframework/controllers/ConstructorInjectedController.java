83c78fc0f2d0cce0bd9cc1882bcb230dbcf050f2
Daniel Modrzejewski <danio1118m@gmail.com>
2019-06-02 13:09:40 +0200

    finish dependency injection using Spring Framework lesson

--- src/main/java/guru/springframework/controllers/ConstructorInjectedController.java
+++ src/main/java/guru/springframework/controllers/ConstructorInjectedController.java
@@ -1,11 +1,15 @@
 package guru.springframework.controllers;
 
 import guru.springframework.services.GreetingService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 
+@Controller
 public class ConstructorInjectedController {
 
     private GreetingService greetingService;
 
+    @Autowired //not necessary but it's a good practise
     public ConstructorInjectedController(GreetingService greetingService) {
         this.greetingService = greetingService;
     }
