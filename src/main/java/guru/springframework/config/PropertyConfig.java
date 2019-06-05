b560fc04ea156bc02f567f1252057fb1102182cb
Daniel Modrzejewski <danio1118m@gmail.com>
2019-06-05 15:15:41 +0200

    lesson property source. Add datasource.properties, propertyConfig and print username to check

---
+++ src/main/java/guru/springframework/config/PropertyConfig.java
@@ -0,0 +1,37 @@
+package guru.springframework.config;
+
+import guru.springframework.examplebeans.FakeDataSource;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
+
+@Configuration
+@PropertySource("classpath:datasource.properties")
+public class PropertyConfig {
+
+    @Value("${dm.username}")
+    String username;
+
+    @Value("${dm.password}")
+    String password;
+
+    @Value("${dm.dburl}")
+    String dburl;
+
+    @Bean
+    public FakeDataSource fakeDataSource(){
+        FakeDataSource fakeDataSource = new FakeDataSource();
+        fakeDataSource.setUsername(username);
+        fakeDataSource.setPassword(password);
+        fakeDataSource.setDburl(dburl);
+        return fakeDataSource;
+    }
+
+    @Bean
+    public static PropertySourcesPlaceholderConfigurer properties(){
+        PropertySourcesPlaceholderConfigurer propertySourcesPlaceholderConfigurer = new PropertySourcesPlaceholderConfigurer();
+        return propertySourcesPlaceholderConfigurer;
+    }
+}
